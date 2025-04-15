d72c48276eabaee5c859105e7e962903e7996175
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2025-04-14 21:25:59 -0400

    End game message

--- InputProcessing.gd
+++ InputProcessing.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var score_label:Node
 @export var instructions_label:Node
+@export var end_message_label:Node
 @export var target_node:Node
 @export var middle_block_tree:PackedScene
 @export var top_block_hair_brown:PackedScene
@@ -12,6 +13,9 @@ var height = 430
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if(Input.is_action_just_pressed("ui_select")):
+		instructions_label.visible = false
+	
 	if(Input.is_action_pressed("ui_select")):
 		timer += delta
 		
@@ -24,14 +28,10 @@ func _process(delta):
 			newInstance.global_position.x = 560
 			newInstance.global_position.y = height
 			height -= 127
-			
-			##Insert pixel art instantiation
+
 			##Move camera n shit
 			
 		score_label.text = str("Your hair is ", score, " mm tall.")
-		
-		if(score >= 1):
-			instructions_label.visible = false
 			
 	elif(Input.is_action_just_released("ui_select")):
 		var newInstance = top_block_hair_brown.instantiate()
@@ -39,7 +39,8 @@ func _process(delta):
 		newInstance.global_position.x = 560
 		newInstance.global_position.y = height
 		set_process(false)
-		print("Game Over")
+		end_message_label.visible = true
+		end_message_label.text = str("Your hair has stopped its growth. \nIts final length is " , score, "mm")
 		
 		
 		
